fdf5539eb5a812150741d18a116fe950a054647a
Olga Kapichevagit config --global user.email kapi4ella@gmail.com@ <kapi4ella@gmail.com>
2023-08-14 21:40:41 +0300

    task_final/

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,14 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: {name, phone}, order: {address, sum}, goods: []};
+    let data = {
+        client: `${name} ${phone}`, 
+        order: {
+            address: "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв. " + address.flat,
+            sum: sum
+        },
+        goods: []
+    };
 
     let countOfGoods = goods.length;
 
@@ -42,12 +49,7 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
-    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв. " + address.flat;
-    data.order.sum = sum;
-
-    data.client = `${name}` + " " + `${phone}`;
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({ data });
 
     return jsonData;
 }
